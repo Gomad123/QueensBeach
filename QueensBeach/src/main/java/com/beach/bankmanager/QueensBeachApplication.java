339e3dbbb2078835d4e9b5411a28b4c722862c48
Gomad123 <madhangopal.kumar@citi.com>
2016-04-13 11:51:14 -0400

    Added files via upload
    First Commit for Queens Boro Beach Project. Hello World

---
+++ QueensBeach/src/main/java/com/beach/bankmanager/QueensBeachApplication.java
@@ -0,0 +1,13 @@
+package com.beach.bankmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueensBeachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueensBeachApplication.class, args);
+		System.out.println("HELLO WORLD");
+	}
+}
