f26b14c29ec060e32b499cc4da183e2318bb4266
Chakraborty <dc43887@nsroot.net>
2016-04-13 16:57:18 -0400

    Addind downstream-stub service for data mocks

---
+++ downstream-stub/src/main/java/com/example/DownstreamStubApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownstreamStubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DownstreamStubApplication.class, args);
+	}
+}
